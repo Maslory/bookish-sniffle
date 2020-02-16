4e248084c13d21299bd5f876b4309e3db415df66
Maslory <stanlisword@gmail.com>
2020-02-17 00:07:08 +0500

    Updates

---
+++ precache-manifest.65ea99205fdd168a97667019477831d1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e7e507deb1cfd0d6877253ddf5c0012",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "25b948c8c650b36196f3",
+    "url": "/my-app/static/css/main.d804049a.chunk.css"
+  },
+  {
+    "revision": "487306598bf225237d99",
+    "url": "/my-app/static/js/2.b45b37db.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/my-app/static/js/2.b45b37db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25b948c8c650b36196f3",
+    "url": "/my-app/static/js/main.fa78277f.chunk.js"
+  },
+  {
+    "revision": "134c6d7b033e4b816dc9",
+    "url": "/my-app/static/js/runtime-main.56c001b7.js"
+  },
+  {
+    "revision": "58bf8e0bd1dd213bf745e9d748caf175",
+    "url": "/my-app/static/media/Roboto.58bf8e0b.woff"
+  },
+  {
+    "revision": "6375717a86bdcd558c522aff052e27d2",
+    "url": "/my-app/static/media/iconmonstr-trash-can-1.6375717a.svg"
+  }
+]);
